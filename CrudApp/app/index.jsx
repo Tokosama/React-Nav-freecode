ae6e6e166e2acbfdb3353cdcc7bfb1fae680fbcb
Tokosama <anouartoko@gmail.com>
2025-08-17 02:04:03 +0100

    Font size and dark  lightt mode

--- CrudApp/app/index.jsx
+++ CrudApp/app/index.jsx
@@ -1,20 +1,41 @@
-import { Text, View, TextInput, Pressable, FlatList, StyleSheet } from "react-native";
+import {
+  Text,
+  View,
+  TextInput,
+  Pressable,
+  FlatList,
+  StyleSheet,
+} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { useState } from "react";
+import { useState, useContext } from "react";
 import { data } from "@/data/todos";
+import { ThemeContext } from "@/context/ThemeContext";
 
-import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
+import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
+
+import { Inter_500Medium, useFonts } from "@expo-google-fonts/inter";
+
+import Octicons from "@expo/vector-icons/Octicons";
 
 export default function Index() {
   const [todos, setTodos] = useState(data.sort((a, b) => b.id - a.id));
   const [text, setText] = useState("");
+  const { colorScheme, setColorScheme, theme } = useContext(ThemeContext);
+  const [loaded, error] = useFonts({
+    Inter_500Medium,
+  });
+
+  if (!loaded && !error) {
+    return null;
+  }
+
+  const styles = createStyles(theme,colorScheme)
 
   const addTodo = () => {
     if (text.trim()) {
       const newId = todos.length > 0 ? todos[0].id + 1 : 1;
       setTodos([{ id: newId, title: text, completed: false }, ...todos]);
       setText("");
-      
     }
   };
 
@@ -30,17 +51,24 @@ export default function Index() {
     setTodos(todos.filter((todo) => todo.id !== id));
   };
 
-
-  const renderItem=({item}) =>(
+  const renderItem = ({ item }) => (
     <View style={styles.todoItem}>
-      <Text style={[styles.todoText, item.completed && styles.completedText]}
-      onPress={()=>toggleTodo(item.id)}
-      >{item.title}</Text>
-      <Pressable onPress={()=>removeTodo(item.id)}>
-        <MaterialCommunityIcons name="delete-circle" size={36} color="red" selectable={undefined} />
+      <Text
+        style={[styles.todoText, item.completed && styles.completedText]}
+        onPress={() => toggleTodo(item.id)}
+      >
+        {item.title}
+      </Text>
+      <Pressable onPress={() => removeTodo(item.id)}>
+        <MaterialCommunityIcons
+          name="delete-circle"
+          size={36}
+          color="red"
+          selectable={undefined}
+        />
       </Pressable>
     </View>
-  )
+  );
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.inputContainer}>
@@ -51,78 +79,97 @@ export default function Index() {
           value={text}
           onChangeText={setText}
         />
-        <Pressable onPress={addTodo} style={styles.addButton}>
+        <Pressable
+          onPress={addTodo}
+          style={styles.addButton}
+        >
           <Text style={styles.addButtonText}>Add</Text>
+        </Pressable>
+        <Pressable
+          onPress={() =>
+            setColorScheme(colorScheme === "light" ? "dark" : "light")
+          }
+          style={{ marginLeft: 10 }}
+        >
+          <Octicons
+            name={colorScheme === "dark" ? "moon" : "sun"}
+            size={36}
+            color={theme.text}
+            selectable={undefined}
+            style={{ width: 36 }}
+          />
         </Pressable>
       </View>
       <FlatList
-      data={todos}
-      renderItem={renderItem}
-      keyExtractor={todo =>todo.id}
-      contentContainerStyle={{flexGrow:1}}
-
+        data={todos}
+        renderItem={renderItem}
+        keyExtractor={(todo) => todo.id}
+        contentContainerStyle={{ flexGrow: 1 }}
       />
     </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
-  container:{
-    flex:1,
-    backgroundColor:'black',
+function createStyles(theme, colorScheme) {
+  return StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: theme.background,
+    },
+    inputContainer: {
+      flexDirection: "row",
+      alignItems: "center",
+      marginBottom: 10,
+      padding: 10,
+      width: "100%",
+      maxWidth: 1024,
+      marginHorizontal: "auto",
+      pointerEvents: "auto",
+    },
+    input: {
+      flex: 1,
+      borderColor: "gray",
+      borderWidth: 1,
+      borderRadius: 5,
+      padding: 10,
+      marginRight: 10,
+      fontSize: 18,
+      fontFamily: "Inter_500Medium",
+      minWidth: 0,
+      color: theme.text,
+    },
+    addButton: {
+      backgroundColor: theme.button,
+      borderRadius: 5,
+      padding: 10,
+    },
+    addButtonText: {
+      fontSize: 18,
+      color:colorScheme=== 'dark'?'black':'white',
+    },
+    todoItem: {
+      flexDirection: "row",
+      alignItems: "center",
+      justifyContent: "space-between",
+      gap: 4,
+      padding: 10,
+      borderBottomColor: "gray",
+      borderBottomWidth: 1,
+      width: "100%",
+      maxWidth: 1024,
+      marginHorizontal: "auto",
+      pointerEvents: "auto",
+    },
+    todoText: {
+      flex: 1,
+      fontSize: 18,
+      fontFamily: "Inter_500Medium",
 
-  },
-  inputContainer:{
-    flexDirection:'row',
-    alignItems:'center',
-    marginBottom:10,
-    padding:10,
-    width:'100%',
-    maxWidth:1024,
-    marginHorizontal:'auto',
-    pointerEvents:'auto',
-  },
-  input:{
-    flex:1,
-    borderColor:'gray',
-    borderWidth:1,
-    borderRadius:5,
-    padding:10,
-    marginRight:10,
-    fontSize:18,
-    minWidth:0,
-    color:'white',
-  },
-  addButton:{
-    backgroundColor:'white',
-    borderRadius:5,
-    padding:10,
-  },
-  addButtonText:{
-    fontSize:18,
-    color:'black',
-  },
-  todoItem:{
-    flexDirection:'row',
-    alignItems:'center',
-    justifyContent:'space-between',
-    gap:4,
-    padding:10,
-    borderBottomColor:'gray',
-    borderBottomWidth:1,
-    width:'100%', 
-    maxWidth:1024,
-    marginHorizontal:'auto',
-    pointerEvents:'auto',
- },
- todoText:{
-  flex:1,
-fontSize:18,
-color:'white',
- },
- completedText:{
-  textDecorationLine:'line-through',
-  color:'gray',
-
- }
-})
+      color: theme.text,
+    },
+    completedText: {
+      textDecorationLine: "line-through",
+      color: "gray",
+    },
+  });
+}
---
+++ CrudApp/constants/Colors.ts
@@ -0,0 +1,17 @@
+
+
+export const Colors = {
+  light: {
+    text: '#11181C',
+    background: '#fff',
+    icon: 'black',
+    button:'royalblue'
+    
+  },
+  dark: {
+    text: 'white',
+    background: 'black',
+    icon: 'red',
+    button:'white',
+  },
+};
